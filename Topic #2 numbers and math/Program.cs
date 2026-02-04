1ef5918e0fab66725b37abc8a56affbc97583ff1
362707242 <362707242@2260I-R31-001>
2026-02-04 13:15:40 -0500

    Add project files.

---
+++ Topic #2 numbers and math/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Topic__2_numbers_and_math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I will now count my chickens");
+            Console.WriteLine("Hens " + (10 + 5 * 2));
+            Console.WriteLine("Roosters " + (25 + 30 / 6));
+            Console.WriteLine("Six" + 6);
+            Console.WriteLine("6 + 6");
+            Console.WriteLine("6" + 6);
+            Console.WriteLine(6 + 6);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Now i will count the eggs");
+            Console.WriteLine(11.0 / 2);
+
+        }
+    }
+}
